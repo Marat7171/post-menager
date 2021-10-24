2d8e514b97abb3bdfe78f641fac7ea4e78d10b3f
Marat Karimov <karimomarat@mail.ru>
2021-10-24 10:29:28 +0300

    v0.3 +search(unfinished)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,6 +13,18 @@ function App() {
     ]);
 
     const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    function getSortedPosts() {
+        console.log('Отработала функция sortedPosts');
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        return posts;
+    }
+
+    const sortedPosts = getSortedPosts()
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
@@ -22,25 +35,31 @@ function App() {
 
     const sortPosts = (sort) => {
         setSelectedSort(sort);
-        setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])));
     }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
-            <MySelect
-                value={selectedSort}
-                onChange={sortPosts}
-                defaultValue="sorting"
-                options={[
-            {value: 'title', name: 'By name'},
-            {value: 'body', name: 'By description'},
-        ]}
-            />
+            <div>
+                <MyInput
+                    placeholder="Search"
+                    value={searchQuery}
+                    onChange={e => setSearchQuery(e.target.value)}
+                />
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="sorting"
+                    options={[
+                        {value: 'title', name: 'By name'},
+                        {value: 'body', name: 'By description'},
+                    ]}
+                />
+            </div>
             {posts.length
                 ?
-                <PostList remove={removePost} posts={posts} title='Posts about JS'/>
+                <PostList remove={removePost} posts={sortedPosts} title='Posts about JS'/>
                 :
                 <h1 style={{textAlign: 'center'}}>
                     Posts not found!
