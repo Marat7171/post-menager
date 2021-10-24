3443a5e5dec34c5b40002f00c4405ee05bcd9ad2
Marat Karimov <karimomarat@mail.ru>
2021-10-24 23:07:32 +0300

    v0.7.5 +useFetching

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostsLoading, setIsPostsLoading] = useState(false)
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    })
+
 
 
     // Если передать массив пустым, то функция отработает только 1 раз
@@ -27,15 +32,6 @@ function App() {
         setModal(false)
     }
 
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        // setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        // }, 5000)
-
-    }
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -50,7 +46,13 @@ function App() {
                 <PostForm create={createPost}/>
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
-            <PostFilter filter={filter} setFilter={setFilter}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            {postError &&
+                <h1>Произошла ошибка ${postError}</h1>
+            }
             {isPostsLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts about JS'/>
